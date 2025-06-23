ca62bff4d67972fbfb685bb3e1768231f0e26e2f
gilangcahyono <gilcah2207@gmail.com>
2025-06-23 07:40:07 +0700

    mengubah database

--- application/views/module/sidebar.php
+++ application/views/module/sidebar.php
@@ -2,10 +2,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= base_url('admin') ?>" class="brand-link">
-      <img src="https://robohash.org/AdminLTELogo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="<?= base_url('assets/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
         style="opacity: .8">
-      <!-- <img src="<?= base_url('assets/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-        style="opacity: .8"> -->
       <span class="brand-text font-weight-light">Dashboard Admin</span>
     </a>
 
